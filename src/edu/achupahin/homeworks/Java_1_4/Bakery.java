bc169f5c850988207932611f044bd0fc416d28cd
achupahin <achupahin@gmail.com>
2016-02-15 10:53:13 +0300

    Publication

--- src/edu/achupahin/homeworks/Java_1_4/Bakery.java
+++ src/edu/achupahin/homeworks/Java_1_4/Bakery.java
@@ -4,46 +4,41 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
 
-
 public class Bakery {
 
- static final Random RANDOM = new Random();
- static final Calendar CALENDAR = Calendar.getInstance();
+ static final Random RANDOM = new Random ();
+ static final Calendar CALENDAR = Calendar.getInstance ();
 
- public static void main(String[] args) {
-  Cake[] cakes=new Cake[10];
+
+ public static void main (String[] args) {
+  Cake[] cakes = new Cake[10];
 
   //Замешиваем
   for (int i = 0, size = -1; i < cakes.length; i++) {
-   int a=i+1;
-   Cake cake = new Cake("Кекс_" + a, Cake.getRandomInt(100,400));
-   cakes[i]=cake;
+   int a = i + 1;
+   Cake cake = new Cake ("Кекс_" + a, Cake.getRandomInt (100, 400));
+   cakes[i] = cake;
   }
   //Выпекаем
-  for (Cake cake: cakes){
-   cake.setWeightTime();
-   System.out.println(String.format("%1$s готов", cake.getName()));
+  for (Cake cake : cakes) {
+   cake.setWeightTime ();
+   System.out.println (String.format ("%1$s готов", cake.getName ()));
   }
   //Выводим данные по кексам
-  for (Cake cake: cakes){
-   System.out.println(cake.toString());
-  }
-  System.out.println("Увеличиваем муку и воду");
-  for (int i = 0; i < cakes.length; i++) {
-   int a=i+1;
-   if (a%2==0) {
-    cakes[i].setFlour(cakes[i].getFlour()+50f);
-    cakes[i].setWeightTime();
-   }
-   else {
-    cakes[i].setWater(cakes[i].getWater()+80f);
-    cakes[i].setWeightTime();
+  for (Cake cake : cakes) System.out.println (cake);
+
+  System.out.println ("Увеличиваем муку и воду");
+  int i = 1;
+  for (Cake cake : cakes) {
+   if (i % 2 == 0) {
+    cake.setFlour (cake.getFlour () + 50f);
+    cake.setWeightTime ();
+   } else {
+    cake.setWater (cake.getWater () + 80f);
+    cake.setWeightTime ();
    }
   }
   //Выводим данные по кексам
-  for (Cake cake: cakes){
-   System.out.println(cake.toString());
-  }
-  //System.out.println(String.format("Время %1$tH:%1$tM", timeOfBake));
+  for (Cake cake : cakes) System.out.println (cake);
  }
 }
